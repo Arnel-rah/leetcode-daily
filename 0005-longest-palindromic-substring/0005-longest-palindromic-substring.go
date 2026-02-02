b88ca22ad3abee1340b965c912731cfd886d4b23
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2026-02-02 11:53:45 +0300

    Time: 3 ms (59.2%), Space: 4.4 MB (63.95%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -0,0 +1,30 @@
+func longestPalindrome(s string) string {
+    if len(s) <= 1 {
+        return s
+    }
+    
+    start, maxLen := 0, 1
+    
+    for i := 0; i < len(s); i++ {
+        l, r := i, i
+        for l >= 0 && r < len(s) && s[l] == s[r] {
+            if r-l+1 > maxLen {
+                start = l
+                maxLen = r - l + 1
+            }
+            l--
+            r++
+        }
+        l, r = i, i+1
+        for l >= 0 && r < len(s) && s[l] == s[r] {
+            if r-l+1 > maxLen {
+                start = l
+                maxLen = r - l + 1
+            }
+            l--
+            r++
+        }
+    }
+    
+    return s[start : start+maxLen]
+}
