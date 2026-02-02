9ba466bfadf2c33a645047d25d868c4bc9ae85b8
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2026-02-02 19:24:03 +0300

    Time: 2 ms (35.08%), Space: 4.2 MB (58.03%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.go
@@ -0,0 +1,29 @@
+func isMatch(s string, p string) bool {
+    m, n := len(s), len(p)
+    dp := make([][]bool, m+1)
+    for i := range dp {
+        dp[i] = make([]bool, n+1)
+    }
+    
+    dp[0][0] = true
+    
+    for j := 2; j <= n; j += 2 {
+        if p[j-1] == '*' {
+            dp[0][j] = dp[0][j-2]
+        }
+    }
+    
+    for i := 1; i <= m; i++ {
+        for j := 1; j <= n; j++ {
+            if p[j-1] == '*' {
+                match := (p[j-2] == '.' || p[j-2] == s[i-1]) && dp[i-1][j]
+                dp[i][j] = dp[i][j-2] || match
+            } else {
+                match := (p[j-1] == '.' || p[j-1] == s[i-1])
+                dp[i][j] = match && dp[i-1][j-1]
+            }
+        }
+    }
+    
+    return dp[m][n]
+}
