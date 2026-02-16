8b59348fd3b14c4e4f841c206b07a48227f6bf17
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2026-02-16 10:19:50 +0300

    Time: 0 ms (100%), Space: 4 MB (22.02%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.go
@@ -0,0 +1,19 @@
+func myPow(x float64, n int) float64 {
+    if n == 0 {
+        return 1
+    }
+    if n < 0 {
+        x = 1 / x
+        n = -n
+    }
+
+    res := 1.0
+    for n > 0 {
+        if n&1 == 1 {
+            res *= x
+        }
+        x *= x
+        n >>= 1
+    }
+    return res
+}
